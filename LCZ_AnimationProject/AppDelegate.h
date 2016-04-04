7e4b1f5fabc03bbfd3fe0172f725aaa95a623844
lxx <listen987654321@sina.com>
2016-04-04 17:58:42 +0800

    Initial Commit

---
+++ LCZ_AnimationProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCZ_AnimationProject
+//
+//  Created by lxx on 16/4/4.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCZ_AnimationProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCZ_AnimationProject
+//
+//  Created by lxx on 16/4/4.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
